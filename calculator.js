9d04d123ec59b7c9435125d7c9c5a9f89179e57c
johnathew <jakornegay01@gmail.com>
2022-07-19 12:39:54 -0500

    only add one operator at a time

--- calculator.js
+++ calculator.js
@@ -1,6 +1,14 @@
 const buttons = document.querySelectorAll("button");
 let result = document.getElementById("result");
 let display = document.getElementById("display");
+let operatorDisplay = document.getElementById("operatorDisplay");
+
+
+let operation = "";
+let firstOperand = "";
+let waitingOnSecondOperand = null
+let secondOperand = "";
+
 
 function add(a, b) {
   return (a += b);
@@ -18,37 +26,73 @@ function divide(a, b) {
   return (a /= b);
 }
 
-let operation = "";
+
 
 function calculate(e) {
-  if (e.value >= 0) {
-    operation += e.innerText;
-    display.textContent = operation;
-  }
+  operation += e.innerText;
+  display.textContent = operation;
+  console.log(operation)
 }
 
-function operator(e) {
-  if (
-    e.id === "plusButton" ||
-    e.id === "minusButton" ||
-    e.id === "divideButton" ||
-    e.id === "timesButton"
-  ) {
-    operation += e.innerText;
-    display.textContent = operation;
 
-    // if (operation.includes("*")) {
-    //     operation.replace("*", " ")
-    //     console.log(operation)
-    // }
-  }
+
+
+function operate(e) {
+
+  const operatorTest = /[+-\/*]/.test(operation)
+
+  if (e.value === "+" && operatorTest === false) {
+    display.textContent += e.value
+    operation += e.value
+  } 
+  if (e.value === "-" && operatorTest === false) {
+    display.textContent += e.value
+    operation += e.value
+  } if (e.value === "*" && operatorTest === false) {
+    display.textContent += e.value
+    operation += e.value
+  } if (e.value === "/" && operatorTest === false) {
+    display.textContent += e.value
+    operation += e.value
+  } 
+
+
+  // let replacePlus = display.textContent.replace("+", function() {})
+  // let replaceMinus = display.textContent.replace("-", "blah")
+  // let replaceDivide = display.textContent.replace("/", "watever")
+  // let replaceTimes = display.textContent.replace("*", "watever")
+
+
+  // display.textContent.includes(plusOperator) ? replacePlus : display.textContent += e.value;
+  // display.textContent.includes(minusOperator) ? replaceMinus : display.textContent += e.value;
+  // display.textContent.includes(divideOperator) ? replaceDivide : display.textContent += e.value;
+  // display.textContent.includes(timesOperator) ? replaceTimes : display.textContent += e.value;
+
+  console.log(display.textContent, "display")
+  console.log(operation,"operation")
+  console.log(operatorTest)
 }
+
+//   if (e.value === "-" && display.textContent.includes("-")) {
+//     display.textContent += e.value;
+//     operation += e.value;
+//   }
+
+//   if (e.value === "/" && display.textContent.includes("/")) {
+//     display.textContent += e.value;
+//     operation += e.value;
+//   }
+//   if (e.value === "*" && !display.textContent.includes("*")) {
+//     display.textContent += e.value;
+//     operation += e.value;
+//   }
+// }
 
 function doMath() {
-  const timesOperator = operation.split("*")
-  const minusOperator = operation.split("-")
-  const addOperator = operation.split("+")
-  const divisionOperator = operation.split("/")
+  const timesOperator = operation.split("*");
+  const minusOperator = operation.split("-");
+  const addOperator = operation.split("+");
+  const divisionOperator = operation.split("/");
 
   if (operation.includes("*")) {
     result.textContent = multiply(+timesOperator[0], +timesOperator[1]);
@@ -94,3 +138,54 @@ function clear() {
 
 //       operation.replace(e.innerText);
 //     }
+
+// e.id === "plusButton" ||
+// e.id === "minusButton" ||
+// e.id === "divideButton" ||
+// e.id === "timesButton"
+
+// switch (e.id) {
+//     case "plusButton":
+//       e.innerText += lastOperator
+//       console.log (lastOperator)
+//       display.textContent += lastOperator
+
+//     //   display.textContent.includes("+")
+//     //     ? display.textContent.replace("+", e.innerText)
+//     //     : (display.textContent += e.innerText);
+
+//     //   operation.includes("+")
+//     //     ? operation.textContent.replace("+", e.innerText)
+//     //     : (operation.textContent += e.innerText);
+
+//       break;
+//     case "minusButton":
+//       display.textContent.includes("-")
+//         ? display.textContent.replace("-", e.innerText)
+//         : (display.textContent += e.innerText);
+
+//       operation.includes("-")
+//         ? operation.textContent.replace("-", e.innerText)
+//         : (operation.textContent += e.innerText);
+
+//       break;
+//     case "timesButton":
+//       display.textContent.includes("*")
+//         ? display.textContent.replace("*", e.innerText)
+//         : (display.textContent += e.innerText);
+
+//       operation.includes("*")
+//         ? operation.textContent.replace("*", e.innerText)
+//         : (operation.textContent += e.innerText);
+
+//       break;
+//     case "divideButton":
+//       display.textContent.includes("/")
+//         ? display.textContent.replace("/", e.innerText)
+//         : (display.textContent += e.innerText);
+
+//       operation.includes("/")
+//         ? operation.textContent.replace("/", e.innerText)
+//         : (operation.textContent += e.innerText);
+//       break;
+//   }
