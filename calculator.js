0c70efb9c2688b9fc607e8628ac6d9e5b9384271
johnathew <jakornegay01@gmail.com>
2022-07-21 18:31:37 -0500

    change operator function added

--- calculator.js
+++ calculator.js
@@ -3,12 +3,10 @@ let result = document.getElementById("result");
 let display = document.getElementById("display");
 let operatorDisplay = document.getElementById("operatorDisplay");
 
-
 let operation = "";
 let firstOperand = "";
-let waitingOnSecondOperand = null
+let waitingOnSecondOperand = false;
 let secondOperand = "";
-
 
 function add(a, b) {
   return (a += b);
@@ -26,67 +24,56 @@ function divide(a, b) {
   return (a /= b);
 }
 
-
-
 function calculate(e) {
   operation += e.innerText;
   display.textContent = operation;
-  console.log(operation)
+
+  console.log(operation);
+
 }
-
-
-
 
 function operate(e) {
+  const operatorTest = /[+-\/*]/.test(operation);
+  if (
+    operatorTest === true &&
+    (display.textContent.charAt(display.innerHTML.length - 1) === "+" ||
+      display.textContent.charAt(display.innerHTML.length - 1) === "-" ||
+      display.textContent.charAt(display.innerHTML.length - 1) === "/" ||
+      display.textContent.charAt(display.innerHTML.length - 1) === "*")
+  ) {
+    waitingOnSecondOperand = true;
+    display.textContent = display.textContent.replace(
+      `${display.textContent.charAt(display.innerHTML.length - 1)}`,
+      e.value
+    );
+    operation = operation.replace(
+      `${operation.charAt(operation.length - 1)}`,
+      e.value
+    );
+  }
 
-  const operatorTest = /[+-\/*]/.test(operation)
-
-  if (e.value === "+" && operatorTest === false) {
-    display.textContent += e.value
-    operation += e.value
-  } 
-  if (e.value === "-" && operatorTest === false) {
-    display.textContent += e.value
-    operation += e.value
-  } if (e.value === "*" && operatorTest === false) {
-    display.textContent += e.value
-    operation += e.value
-  } if (e.value === "/" && operatorTest === false) {
-    display.textContent += e.value
-    operation += e.value
-  } 
-
-
-  // let replacePlus = display.textContent.replace("+", function() {})
-  // let replaceMinus = display.textContent.replace("-", "blah")
-  // let replaceDivide = display.textContent.replace("/", "watever")
-  // let replaceTimes = display.textContent.replace("*", "watever")
-
-
-  // display.textContent.includes(plusOperator) ? replacePlus : display.textContent += e.value;
-  // display.textContent.includes(minusOperator) ? replaceMinus : display.textContent += e.value;
-  // display.textContent.includes(divideOperator) ? replaceDivide : display.textContent += e.value;
-  // display.textContent.includes(timesOperator) ? replaceTimes : display.textContent += e.value;
-
-  console.log(display.textContent, "display")
-  console.log(operation,"operation")
-  console.log(operatorTest)
+  if (operatorTest === false || waitingOnSecondOperand === false) {
+    firstOperand += e.value;
+    switch (e.value) {
+      case "+":
+        display.textContent += firstOperand;
+        operation += firstOperand;
+        break;
+      case "-":
+        display.textContent += firstOperand;
+        operation += firstOperand;
+        break;
+      case "*":
+        display.textContent += firstOperand;
+        operation += firstOperand;
+        break;
+      case "/":
+        display.textContent += firstOperand;
+        operation += firstOperand;
+        break;
+    }
+  }
 }
-
-//   if (e.value === "-" && display.textContent.includes("-")) {
-//     display.textContent += e.value;
-//     operation += e.value;
-//   }
-
-//   if (e.value === "/" && display.textContent.includes("/")) {
-//     display.textContent += e.value;
-//     operation += e.value;
-//   }
-//   if (e.value === "*" && !display.textContent.includes("*")) {
-//     display.textContent += e.value;
-//     operation += e.value;
-//   }
-// }
 
 function doMath() {
   const timesOperator = operation.split("*");
@@ -112,80 +99,3 @@ function clear() {
   result.textContent = "";
   display.textContent = "";
 }
-
-// if (
-//     !operation.includes("*") ||
-//     !operation.includes("-") ||
-//     !operation.includes("+") ||
-//     !operation.includes("/")
-//   ) {
-//     if (
-//       e.id === "plusButton" ||
-//       e.id === "minusButton" ||
-//       e.id === "divideButton" ||
-//       e.id === "timesButton"
-//     ) {
-//       operation += e.innerText;
-//       display.textContent += e.innerText;
-//     }
-// }
-//     if (
-//       operation.includes("*") ||
-//       operation.includes("-") ||
-//       operation.includes("+") ||
-//       operation.includes("/")
-//     ) {
-
-//       operation.replace(e.innerText);
-//     }
-
-// e.id === "plusButton" ||
-// e.id === "minusButton" ||
-// e.id === "divideButton" ||
-// e.id === "timesButton"
-
-// switch (e.id) {
-//     case "plusButton":
-//       e.innerText += lastOperator
-//       console.log (lastOperator)
-//       display.textContent += lastOperator
-
-//     //   display.textContent.includes("+")
-//     //     ? display.textContent.replace("+", e.innerText)
-//     //     : (display.textContent += e.innerText);
-
-//     //   operation.includes("+")
-//     //     ? operation.textContent.replace("+", e.innerText)
-//     //     : (operation.textContent += e.innerText);
-
-//       break;
-//     case "minusButton":
-//       display.textContent.includes("-")
-//         ? display.textContent.replace("-", e.innerText)
-//         : (display.textContent += e.innerText);
-
-//       operation.includes("-")
-//         ? operation.textContent.replace("-", e.innerText)
-//         : (operation.textContent += e.innerText);
-
-//       break;
-//     case "timesButton":
-//       display.textContent.includes("*")
-//         ? display.textContent.replace("*", e.innerText)
-//         : (display.textContent += e.innerText);
-
-//       operation.includes("*")
-//         ? operation.textContent.replace("*", e.innerText)
-//         : (operation.textContent += e.innerText);
-
-//       break;
-//     case "divideButton":
-//       display.textContent.includes("/")
-//         ? display.textContent.replace("/", e.innerText)
-//         : (display.textContent += e.innerText);
-
-//       operation.includes("/")
-//         ? operation.textContent.replace("/", e.innerText)
-//         : (operation.textContent += e.innerText);
-//       break;
-//   }
